c29f4d0422382e4865c9bdafa39432d76b892689
Daniel Delgado <dedd1993@gmail.com>
2020-04-23 17:45:56 -0500

    adding nodemon

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my API!')
+  res.send('Welcome to my API!');
 });
 
 app.listen(port, () => {
-    console.log('Running on port ' + port);
+  console.log('Running on port ' + port);
 });
