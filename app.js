d3274762e6f8427b8054b7436ec7e0ded07f19d7
Daniel Delgado <dedd1993@gmail.com>
2020-04-23 20:20:13 -0500

    code cleanup

--- app.js
+++ app.js
@@ -4,41 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 8080;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRoutes')(Book);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    })
-  });
-
-  bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    })
-  });
 
 app.use('/', bookRouter);
 
---
+++ routes/bookRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+function routes(Book) {
+  const bookRouter = express.Router();
+
+  bookRouter.route('/books')
+    .post((req, res) => {
+      const book = new Book(req.body);
+      book.save();
+      return res.status(201).json(book);
+    })
+    .get((req, res) => {
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      })
+    });
+
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      })
+    });
+
+  return bookRouter;
+};
+
+module.exports = routes;
