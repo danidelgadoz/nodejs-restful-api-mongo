e3bdfdb8ace7d3672a24f10958e2a5ca6a5610e2
Daniel Delgado <dedd1993@gmail.com>
2020-04-23 21:10:39 -0500

    implementing PATCH

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -42,9 +42,30 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      return res.json(book);
-    });
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      })
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
 
   return bookRouter;
 };
