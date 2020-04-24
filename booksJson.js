7faf1f4943cbd000d999f151cff31958eac1cb1a
Daniel Delgado <dedd1993@gmail.com>
2020-04-23 19:19:09 -0500

    wiring up to MongoDB

---
+++ booksJson.js
@@ -0,0 +1,20 @@
+db.books.insertMany([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Los Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'Avengers',
+    genre: 'Historical Fiction',
+    author: 'I dont know',
+    read: false
+  },
+])
