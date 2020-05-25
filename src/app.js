20bbced1472fe38d61447aaf3c6397c924760f6b
Daniel Delgado <dedd1993@gmail.com>
2020-05-24 22:06:57 -0500

    add rquest logger in terminal

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const logger = require('morgan');
 
 mongoose.connect('mongodb://localhost/bookAPI');
 
 const app = express();
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use((req, res, next) => {
