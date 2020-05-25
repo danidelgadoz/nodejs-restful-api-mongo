9a404b2659c48b3ff5f3af6e25215d57a17a9cd0
Daniel Delgado <dedd1993@gmail.com>
2020-05-24 23:53:33 -0500

    add db host environment

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 To Import Book data into yourmongoDB database.
 Make sure mongoDB is running then
-run'mongo bookAPI < backupQuery.js' from the command line
+run 'mongo bookAPI < dbSeeder.js' from the command line
--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const logger = require('morgan');
 
-mongoose.connect('mongodb://localhost/bookAPI');
+const dbHost = process.env.DATA_BASE_HOST || 'localhost';
+mongoose.connect(`mongodb://${dbHost}/bookAPI`);
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Content-Length, Content-Type, Authorization, Access-Control-Allow-Methods');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Content-Length, Content-Type, Authorization, Access-Control-Allow-Methods');
   next();
 });
 
